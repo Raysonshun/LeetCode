e5f71cde185474574e0b4ce22c934140f677c585
Raysonshun <luxinruipsu@gmail.com>
2022-03-01 00:01:21 -0800

    just first leetcode pushed by VS

---
+++ leetcode-17.py
@@ -0,0 +1,39 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+
+        n = len(digits)
+
+        summ = {'2':['a','b','c'],'3':['d','e','f'],'4':['g','h','i'],'5':['j','k','l'],'6':['m','n','o'],'7':['p','q','r','s'],'8':['t','u','v'],'9':['w','x','y','z']}
+
+        res = []
+
+        if n == 1:
+            return summ[digits[0]]
+        if n == 0:
+            return []
+
+        marker = 1
+
+        for i in digits:
+
+            digit = summ[i]
+            tmp = res.copy()
+
+
+            if len(res) == 0:
+
+                for x in digit:
+                    res.append(x)
+
+            else:
+                for k in tmp:
+                    for char in digit:
+                        res.append(k + char)
+
+        n2 = len(res)
+        res2 = []
+
+        for i in range(n2):
+            if len(res[i]) == n:
+                res2.append(res[i])
+        return res2
