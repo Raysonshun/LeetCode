fa98b8f85cba991213c4fc7dd53571ff9b5f8400
Raysonshun <luxinruipsu@gmail.com>
2022-03-27 18:31:00 -0700

    Search in Binary Search Tree

---
+++ leetcode-700.py
@@ -0,0 +1,13 @@
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+
+        if not root:
+            return None
+
+        if root.val == val:
+            return root
+
+        if root.val > val:
+            return self.searchBST(root.left,val)
+        elif root.val < val:
+            return self.searchBST(root.right,val)
