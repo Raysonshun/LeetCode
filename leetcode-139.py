055b07bb87bd9fb3fbccb322ec0d937de3e41551
Raysonshun <luxinruipsu@gmail.com>
2022-05-18 01:30:27 -0700

    Dynamic programming - word break 分词

---
+++ leetcode-139.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+
+        n = len(s)
+
+
+        dp = [False for i in range(n+1)]
+        dp[0] = True
+
+
+        for i in range(1,n+1):
+            for j in range(i):
+                if dp[j] is True and s[j:i] in wordDict:
+                    dp[i] = True
+
+        return dp[-1]
